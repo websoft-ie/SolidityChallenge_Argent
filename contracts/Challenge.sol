0426c21ff9eb6a39888db8ffbc3c88c70df11a77
John Carter <cadsoft.dev@gmail.com>
2022-02-10 18:19:36 +0900

    contract

---
+++ contracts/Challenge.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.8.0;
+
+contract Challenge {
+
+    mapping (bytes32 => uint256) internal _applications;
+
+    function applyUsingEmail(bytes32 emailHash) external {
+        if (_applications[emailHash] != 0) {
+            revert("already applied");
+        }
+        _applications[emailHash] = block.timestamp;
+    }
+
+    function getApplicationID(string memory email) external view returns (uint256) {
+        bytes32 emailHash = keccak256(abi.encodePacked(email));
+        return _applications[emailHash];
+    }
+}
